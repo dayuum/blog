586ab1eb50dbfb30fe8626156c4bc0d1a11f66c4
dayuum <muscat13270@gmail.com>
2021-03-24 14:29:50 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </nav>
+      </header>
+      <main>
+        <a href=""></a>
+        <section>
+          <p>
+            <img src="" alt="" />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente hic provident dolores optio nostrum, odio reiciendis voluptas ex temporibus ut qui corrupti itaque unde quis fugiat accusantium iste velit quasi?
+            Laboriosam voluptatem, praesentium id iusto nostrum labore odit sapiente quas accusamus architecto doloribus ut distinctio dolorem neque in similique dicta a voluptate molestiae nesciunt obcaecati. Dignissimos, nisi? Explicabo, mollitia nostrum.
+            Distinctio sapiente quasi nobis exercitationem numquam expedita aperiam ducimus magni, enim facere amet explicabo repellat dolorem porro molestiae voluptatibus ipsa sint repudiandae optio doloremque quidem. Nam obcaecati quam iste nihil!
+          </p>
+        </section>
+      </main>
+      <footer>
+        <section>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </section>
+        <section>
+          <form>
+            <input type="text" />
+            <button></button>
+          </form>
+        </section>
+      </footer>
+    </>
+  );
+}
+
+export default App;
